bc1a8981be7f27c14aef55af77b4ef03959b74c7
kimyeonkyu7453 <hjk902902@naver.com>
2024-06-03 13:13:23 +0900

    combine_segments.py

--- openpose/pose_lib/combine_segments.py
+++ openpose/pose_lib/combine_segments.py
@@ -1,10 +1,18 @@
 import glob
+import os
 
-output_file = '/front/openpose/pose_lib/pose_iter_160000.caffemodel'
-input_pattern = '/front/openpose/pose_lib/segment_*'
+def combine_files():
+    segment_dir = '/app/openpose/pose_lib'
+    output_file = os.path.join(segment_dir, 'pose_iter_160000.caffemodel')
+    input_pattern = os.path.join(segment_dir, 'segment_*')
 
-with open(output_file, 'wb') as f:
-    for part in sorted(glob.glob(input_pattern)):
-        with open(part, 'rb') as p:
-            f.write(p.read())
+    with open(output_file, 'wb') as f:
+        for part in sorted(glob.glob(input_pattern)):
+            with open(part, 'rb') as p:
+                f.write(p.read())
 
+    # Set the correct permissions
+    os.chmod(output_file, 0o777)
+
+if __name__ == '__main__':
+    combine_files()
